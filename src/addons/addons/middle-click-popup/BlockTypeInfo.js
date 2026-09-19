2b3b143e514b91782451e16b1fd79f3f338d0a97
Lu Yifei <118034848+LuYifei2011@users.noreply.github.com>
2026-09-19 09:31:12 +0800

    feat: integrate boolean toggle shadows

--- src/addons/addons/middle-click-popup/BlockTypeInfo.js
+++ src/addons/addons/middle-click-popup/BlockTypeInfo.js
@@ -424,6 +424,8 @@ export class BlockTypeInfo {
       } else {
         if (!field.argType_) {
           if (field.getText().trim().length !== 0) parts.push(field.getText());
+        } else if (field.argType_.includes("boolean")) {
+          addInput(new BlockInputBoolean(inputIdx, fieldIdx));
         } else if (field.argType_[0] === "colour") {
           addInput(new BlockInputColour(inputIdx, fieldIdx));
         } else if (field.argType_[1] === "number") {
